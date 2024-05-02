f18dade90c52ec145a2194ff24b1246a73410bf3
Kareemrreda <106902105+Kareemrreda@users.noreply.github.com>
2024-05-02 20:56:40 +0300

    Add files via upload

--- eepromfunc.c
+++ eepromfunc.c
@@ -1,12 +1,12 @@
 #include "eeprom.c"
-   
+
     // Initialize EEPROM
     EEPROMInit();
-      
-			void saveCoordinatesToEEPROM(float *latitude, float *longitude) {
+
+			void saveCoordinatesToEEPROM(float latitude, float longitude) {
     // Save latitude to EEPROM (block number 0, address 0)
-    eeprom_write(*latitude, 0, 0);
+    eeprom_write(latitude, 0, 0);
 
     // Save longitude to EEPROM (block number 0, address 4)
-    eeprom_write(*longitude, 0, 4);
+    eeprom_write(longitude, 0, 4);
 }
