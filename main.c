f8e317ceb67d01c0979d37d6a9ae9a28e18aea20
mh2888387 <113148935+mh2888387@users.noreply.github.com>
2024-05-16 03:58:02 +0300

    Revert "Add files via upload"
    This reverts commit a205580d2e8dd7d207e2f5509077b9da5c141d00.

--- main.c
+++ main.c
@@ -1,28 +1,144 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void savingcsv(float *lat, float *lon, int n) {
-    FILE *fp;
-    char filename[] = "data.csv";
-
-    fp = fopen(filename, "w+");
+<<<<<<< HEAD
 
 
-    while (n--) {
-        printf("%f , %f\n", *lon, *lat);
-        fprintf(fp, "%f , %f\n", *lat, *lon);
-        lat++;
-        lon++;
+
+=======
+#include "decoder.h"
+#include "CALC_DISP.h"
+#include "GPIO_DRIVER.H"
+#include "UART_INIT.h"
+#include <math.h>
+#include "NMEA.h"
+//#include <TM4C123.h>
+
+
+
+
+
+
+
+double Start_long; 
+double Start_lat; 
+
+double current_long ;
+double current_lat ;
+
+double prev_long;
+double prev_lat;
+
+
+int inc_distance;
+int total_distance;
+void RGB_SetOutput(unsigned char data){
+    GPIO_PORTF_DATA_R |= data;
+}
+
+void RGB_ClearOutput(unsigned char data){
+    GPIO_PORTF_DATA_R &= data;
+}
+void UART_OutChar(char data){
+    while((UART0_FR_R & 0x20)!=0); // wait for FIFO in transmit TXFF to not be equal 0, not full, to avoid OE error
+    UART0_DR_R= data;
     }
-
-    fclose(fp);
+void UART_OutString(char *pt){
+    while(*pt){
+        UART_OutChar(*pt);
+        pt++;
+    }
 }
 
-int main() {
-    float lat[] = {11.02354,4536};
-    float lon[] = {22.0325,435.5324};
+int main (void){
+	 
+	
+	SWITCH_INIT();
+	LED_INIT();
+	UART_INIT();
+	RGB_SetOutput(0x02);
+	
+	while(1){
+		//---this whole commented section will be used once we finish all drivers in second milestone---
+		//---there is some documentaion to understand the flow of the function---
+		
+		/*
+		//Start_long =READ_UART(); 			 				//we will read constant coordinateof starting position once tiva is turned on  
+		//Start_lat =READ_UART();  
 
-    savingcsv(lat, lon, 2);
-    return 0;
+		current_long = Start_long;  						// assign current position as starting position
+		current_lat = Start_lat;
+
+		total_distance =0; 											// intialize the total distance
+
+		while (total_distance<100){    
+		
+		prev_lat = current_lat;  								// we need to update our cordinates without losing the prev cordinates
+		prev_long = current_long;
+		
+		// delay 2 seconds using systick 				// delay to wait for tiva c to change position
+
+		//current_long =READ_UART();						// we update current position 
+		//current_lat =READ_UART();  
+
+		 inc_distance = calc_displacment(prev_lat, prev_long, current_lat, current_long, 'm'); // we measure the small distance between current and prev position
+		
+		 total_distance += inc_distance; 				// we add inc displacment to total distance
+   		
+     		 if(sw1_read()){						//this function  should be edited to be implemented using interrupts
+		 LED_ON(RED_LED);
+		 break;
+		 }
+}
+*/
+
+//this is just a driver to test if there is any errors
+/*
+total_distance=150;
+ 
+if (total_distance>=100){
+	 LED_ON(GREEN_LED);
+}*/
+float latitude_value=1.5;
+char buffer[100];
+char copy[100];
+char latitude_direction;
+char longitude_direction;
+float longitude_value=1.5;
+float temp_value;
+
+ GPS_GET_LINE(buffer);
+ UART_OutString(buffer);
+ refill(buffer,copy);
+ longitude_direction=getlongitudeDirection(copy);
+  refill(buffer,copy);
+ latitude_direction=getLatitudeDirection(copy);
+  refill(buffer,copy);
+ temp_value=getLatitude(copy);
+ if(temp_value!=0)
+     latitude_value=temp_value; //sets latitude if GPGGA
+  refill(buffer,copy);
+ temp_value=getlongitude(copy);
+ if(temp_value!=0)
+     longitude_value=temp_value;
+  refill(buffer,copy);
+
+  if(latitude_value>1.5){
+     RGB_ClearOutput(~0x0E);
+     RGB_SetOutput(0x06);
+  }
+ /*for(i=0;i<100;i++)
+ {
+	 if(buffer[i]!=0)
+	 WRITE_UART(buffer[i]);
+ }*/
+	}
+	
+
 }
 
+
+
+
+
+
+
+
+>>>>>>> 407b50c (Initialize UART5)
